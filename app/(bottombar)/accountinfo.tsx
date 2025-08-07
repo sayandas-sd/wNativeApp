b80aa839f936f6f626dac49d1bf1a3b95832901c
sayandas-sd <sayandasmac@gmail.com>
2025-08-07 22:15:39 +0530

    feat: add navigation tabs and stack

---
+++ app/(bottombar)/accountinfo.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Accountinfo() {
+    return <View>
+        <Text >
+            account information
+        </Text>
+    </View>
+}
---
+++ app/(tabs)/account.tsx
@@ -0,0 +1,9 @@
+import { SafeAreaView, Text } from "react-native";
+
+export default function account() {
+    return <SafeAreaView style={{backgroundColor: 'green'}}>
+     
+        <Text>account</Text>
+        
+    </SafeAreaView>
+}
